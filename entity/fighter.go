2a3f763a49ac4e9a1d5d1515a4ef196317c548c4
taiki yamashita <taikity0808@gmail.com>
2022-05-06 16:16:05 +0900

    add: field comment

--- entity/fighter.go
+++ entity/fighter.go
@@ -3,13 +3,13 @@ package entity
 import "time"
 
 type Fighter struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Type        string    `json:"type"`
-	Results     string    `json:"results"`
-	Description string    `json:"description"`
-	Backbone    string    `json:"backbone"`
-	Age         int       `json:"age"`
+	ID          string    `json:"id"`          //格闘家ID
+	Name        string    `json:"name"`        //格闘家名
+	Type        string    `json:"type"`        //格闘技の種類 例）総合格闘技
+	Results     string    `json:"results"`     //戦績
+	Description string    `json:"description"` //選手の説明
+	Backbone    string    `json:"backbone"`    //選手のバックボーン
+	Age         int       `json:"age"`         //年齢
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
 }
