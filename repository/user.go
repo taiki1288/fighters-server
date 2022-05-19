04f69f80af86b80646d0462bcd4026d0dbf37275
taiki yamashita <taikity0808@gmail.com>
2022-05-20 02:14:00 +0900

    feat: create NewUserRepository function

--- repository/user.go
+++ repository/user.go
@@ -5,3 +5,10 @@ import "database/sql"
 type UserRepository struct {
 	db *sql.DB
 }
+
+// NewUserRepositoryを初期化
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
