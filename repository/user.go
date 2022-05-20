7e2783303a0186602ddfaee1d2593dc499493e8a
taiki yamashita <taikity0808@gmail.com>
2022-05-20 23:51:48 +0900

    feat: create FindByUserID function in the repository

--- repository/user.go
+++ repository/user.go
@@ -9,6 +9,7 @@ import (
 
 type User interface {
 	CreateUser(ctx context.Context, user *entity.User) error
+	FindByUserID(ctx context.Context, userID string) (*entity.User, error)
 }
 
 type UserRepository struct {
@@ -48,4 +49,28 @@ func (repo *UserRepository) CreateUser(ctx context.Context, user *entity.User) e
 	)
 
 	return nil
+}
+
+func (repo *UserRepository) FindByUserID(ctx context.Context, userID string) (*entity.User, error) {
+	const (
+		find = `SELECT id, name, self_introduction, like_fighters, created_at, updated_at FROM users WHERE id = ?`
+	)
+	row := repo.db.QueryRow(find, userID)
+
+	user := &entity.User{}
+
+	err := row.Scan(
+		&user.ID,
+		&user.Name,
+		&user.SelfIntroduction,
+		&user.Age,
+		&user.LikeFighters,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
